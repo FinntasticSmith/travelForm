4bc68be94acbe26609735420876dcebc82bb41ed
FinntasticSmith <114696609+FinntasticSmith@users.noreply.github.com>
2023-02-01 06:38:06 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+
+  function submitForm() {
+    const firstName = document.getElementById("firstName").value;
+    const lastName = document.getElementById("lastName").value;
+    const age = document.getElementById("age").value;
+    const gender = document.getElementById("gender").value;
+    const destination = document.getElementById("destination").value;
+    const diet = document.getElementById("diet").value;
+
+  
+
+  alert("First Name: " + firstName + "\nLast Name: " + lastName + "\nAge: " + age + "\nGender: " + gender);
+}
